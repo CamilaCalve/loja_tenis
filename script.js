fd00c67c89f592e42472c95bbfa397b89cb09080
Camila Calve Jorge <calvejorge06@gmail.com>
2025-10-16 16:09:54 -0300

    .

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/*peguei ajuda da internet*/
+function abrirPopup(pagina) {
+  document.getElementById('janela').src = pagina;
+  document.getElementById('popup').style.display = 'flex'; // muda para display flex
+}
+
+/*peguei ajuda da internet*/
+function fecharPopup() {
+  document.getElementById('popup').style.display = 'none';
+}
 
 
 
---
+++ scripttenis.js
@@ -0,0 +1,5 @@
+function Selecionar(botao) {
+    botao.classList.toggle('selecionado');
+}
+
+// qualquer elemento que tenha as duas classes botao e selecionado ao mesmo tempo”
